4ce55291cfa4cd285bdf9af2674d9a19e1f006f1
Michael Mächtel <maechtel@htwg-konstanz.de>
2017-10-16 15:11:42 +0200

    INIT: task4

---
+++ hw1/task4/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn square_of_sum(n: i32) -> i32 {
+    unimplemented!();
+}
+
+pub fn sum_of_squares(n: i32) -> i32 {
+    unimplemented!();
+}
+
+pub fn difference(n: i32) -> i32 {
+    unimplemented!();
+}
