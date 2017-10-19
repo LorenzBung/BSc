974e66965d26609602f1b6b9fa15470b9424d546
Lorenz Bung <lorenz.bung@googlemail.com>
2017-10-19 11:54:18 +0200

    Added unimplemented methods in task5 and ran the tests

--- hw1/task4/src/lib.rs
+++ hw1/task4/src/lib.rs
@@ -1,11 +1,19 @@
 pub fn square_of_sum(n: i32) -> i32 {
-    unimplemented!();
+    let mut i: i32 = 0;
+    for x in 1..n+1 {
+        i += x;
+    }
+    i.pow(2)
 }
 
 pub fn sum_of_squares(n: i32) -> i32 {
-    unimplemented!();
+    let mut i: i32 = 0;
+    for x in 1..n+1 {
+       i += x.pow(2); 
+    }
+    i
 }
 
 pub fn difference(n: i32) -> i32 {
-    unimplemented!();
+    square_of_sum(n) - sum_of_squares(n)
 }
