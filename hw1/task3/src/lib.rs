6728f0e1f031abf634e90ad4a36d314b72e21ebc
Lorenz Bung <lorenz.bung@googlemail.com>
2017-10-19 12:05:10 +0200

    Ran rustfmt on all tasks.

--- hw1/task3/src/lib.rs
+++ hw1/task3/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn count(line: &str, c: char) -> u64 {
-   let mut occurances = 0;
-   for char in line.chars(){
-      if char == c {
-        occurances += 1;
-      }
-   }
-   occurances
+    let mut occurances = 0;
+    for char in line.chars() {
+        if char == c {
+            occurances += 1;
+        }
+    }
+    occurances
 }
