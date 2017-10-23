2de89097cd74d9c81c033b224ec438591c9a0c8f
Joshua Rutschmann <joshua.rutschmann@gmx.de>
2017-10-23 12:20:31 +0200

    Ran ci/run-all.sh, formatted hw1/task4 AGAIN :D

--- hw1/task4/src/lib.rs
+++ hw1/task4/src/lib.rs
@@ -8,8 +8,8 @@ pub fn square_of_sum(n: i32) -> i32 {
 
 pub fn sum_of_squares(n: i32) -> i32 {
     let mut i: i32 = 0;
-    for x in 1..n+1 {
-       i += x.pow(2);
+    for x in 1..n + 1 {
+        i += x.pow(2);
     }
     i
 }
