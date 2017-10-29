79e3cba20310afe6bc78153aef65b31fa61863b3
Lorenz Bung <lorenz.bung@googlemail.com>
2017-10-29 13:11:04 +0100

    Added a working task1 and ran tests.

--- hw2/task1/src/lib.rs
+++ hw2/task1/src/lib.rs
@@ -6,12 +6,15 @@ mod tests {
     }
 }
 pub fn hamming_distance(s1: &str, s2: &str) -> Result<usize, String> {
+    //Check if the given Strings are of different length
     if s1.len() != s2.len() {
         return Err("Strings must be of equal length!".to_string());
     }
     let mut dist: usize = 0;
     for i in 0..s1.len() {
+        //Compare each character of the Strings
         if s1.chars().nth(i) != s2.chars().nth(i) {
+            //If they don't match, increment hamming distance
             dist += 1
         }
     }
