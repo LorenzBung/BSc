5347553d919578f527c308ac0c78e36bd9fc4cef
Lorenz Bung <lorenz.bung@googlemail.com>
2017-11-08 13:44:59 +0100

    Added methods in readproc.rs. Some need review.

--- hw4/task1/src/main.rs
+++ hw4/task1/src/main.rs
@@ -4,6 +4,33 @@ mod readproc;
 mod pstree;
 
 fn main() {
-    println!("Hello, world!");
-    println!("{:?}", readproc::self_pids());
+
+    // PID and PPID
+    let pid_tuple = readproc::self_pids().unwrap();
+    let pid = pid_tuple.0;
+    let ppid = pid_tuple.1;
+
+    // Commands
+    let pid_command = readproc::get_pid_command(pid).unwrap();
+    let ppid_command = readproc::get_pid_command(ppid).unwrap();
+
+    // Threads
+    let pid_threads = readproc::get_thread_count(pid).unwrap();
+    let ppid_threads = readproc::get_thread_count(ppid).unwrap();
+
+    // Output for PID and PPID Information
+    println!("My PID : {} - {} running {} threads", pid, pid_command, pid_threads);
+    println!("My PPID: {} - {} running {} threads", ppid, ppid_command, ppid_threads);
+
+    // Memory
+    let vspace = readproc::get_ownprocess_mem().unwrap().0;
+    let code = readproc::get_ownprocess_mem().unwrap().1;
+    let data = readproc::get_ownprocess_mem().unwrap().2;
+    println!("My mem : {} (vspace), {} (code), {} (data)", vspace, code, data);
+
+    // Last Process
+    println!("Last process created in system was: {}", readproc::get_last_created_command().unwrap());
+
+    // Number of tasks
+    println!("Total number of tasks: {}", readproc::get_task_total().unwrap());
 }
--- hw4/task1/src/readproc.rs
+++ hw4/task1/src/readproc.rs
@@ -1,31 +1,53 @@
-use procinfo::pid::Stat;
 use procinfo::pid;
+use procinfo::loadavg;
 
 pub fn self_pids() -> Result<(i32, i32), &'static str>{
     match pid::stat_self() {
         Ok(stat) => { Ok((stat.pid, stat.ppid)) }
-        Err(_) => { Err("Fehler") }
+        Err(_) => { Err("PID not alive: PID and PPID not found") }
     }
 }
 
 pub fn get_pid_command(pid: i32) -> Result<String, &'static str> {
     match pid::stat(pid) {
         Ok(stat) => { Ok(stat.command) }
-        Err(_) => { Err("Fehler") }
+        Err(_) => { Err("PID not alive: no command name found") }
     }
 }
 
-pub fn get_last_created_command() -> Result<String, &'static str> {
-    match pid::stat_self() {
-        Ok(stat) => { Ok(stat.command) }
-        Err(_) => { Err("Fehler") }
+/*pub fn get_last_created_command() -> Result<String, &'static str> {
+    match loadavg() {
+        Ok(stat) => {
+            let last_pid = stat.last_created_pid;
+            match pid::stat(last_pid) {
+                Ok(st) => { Ok(st.command) }
+                Err(_) => { Err("No last command via PID found") }
+            }
+        }
+        Err(_) => { Err("No last command via PID found") }
     }
-
-}
+}*/
 
 pub fn get_thread_count(pid: i32) -> Result<u32, &'static str> {
     match pid::stat(pid) {
         Ok(stat) => { Ok(stat.num_threads as u32) }
-        Err(_) => { Err("Fehler") }
+        Err(_) => { Err("PID not alive: no threads counted") }
+    }
+}
+
+pub fn get_task_total() -> Result<u32, &'static str> {
+    match loadavg() {
+        Ok(stat) => { Ok(stat.tasks_total) }
+        Err(_) => { Err("No total count of tasks in system found") }
+    }
+}
+
+pub fn get_ownprocess_mem() -> Result<(usize, usize, usize), &'static str> {
+    match pid::stat_self() {
+        Ok(stat) => {
+            let csize = stat.end_code - stat.start_code;
+            let dsize = stat.end_data - stat.start_data;
+            Ok((stat.vsize, csize, dsize)) }
+        Err(_) => { Err("PID not alive: no memory found") }
     }
 }
