acc5027303a39b8e2d342e3d184448dd65233379
Lorenz Bung <lorenz.bung@googlemail.com>
2017-12-12 15:50:22 +0100

    Updated Markdown on S1 and fixed typos in task3.

--- hw1/task3/src/lib.rs
+++ hw1/task3/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn count(line: &str, c: char) -> u64 {
-    let mut occurances = 0;
+    let mut occurrences = 0;
     for char in line.chars() {
         if char == c {
-            occurances += 1;
+            occurrences += 1;
         }
     }
-    occurances
+    occurrences
 }
