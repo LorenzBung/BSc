5a5c9bbe339733146f84c1e2c435e29423640078
lorenz <lorenz.bung@googlemail.com>
2020-04-01 18:52:53 +0200

    Fix more deprecated methods

--- hw9/task2/srv-commands/src/lib.rs
+++ hw9/task2/srv-commands/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn parse(message: &str) -> Result<Command, ParseError> {
-    let m: String = String::from(message.trim_right());
+    let m: String = String::from(message.trim_end());
     let mut line = m.lines();
     match line.next() {
         Some(x) => {
             let mut str = x.split_whitespace();
             match str.next() {
                 Some("STAGE") => {
-                    let msg = m[6..].trim_left();
+                    let msg = m[6..].trim_start();
                     Ok(Command::Stage(msg.to_string()))
                 }
                 Some("CONTROL") => {
-                    let cmd = m[8..].trim_left();
+                    let cmd = m[8..].trim_start();
                     Ok(Command::Control(cmd.to_string()))
                 }
                 Some("RETRIEVE") => Ok(Command::Retrieve),
