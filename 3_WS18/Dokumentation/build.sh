2a74d43a669f0b96db1435c9573fd6b1bb03c924
Lorenz Bung <lorenz.bung@googlemail.com>
2020-07-23 10:31:16 +0200

    Move Doc to correct semester

---
+++ 3_WS18/Dokumentation/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+latexmk -xelatex main.tex
--- clear.sh
+++
@@ -1,4 +0,0 @@
-# This file will remove any files created during the PDF creation.
-
-rm -rf *.log *.aux *.lof *.lol *.lot *.out *.synctex.gz *.toc
-rm -rf */*.log */*.aux
